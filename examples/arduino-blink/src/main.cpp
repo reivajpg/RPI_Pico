bf190c80c08433c26a319621fbce3f68a405ba5e
reivajpg <59809259+reivajpg@users.noreply.github.com>
2023-07-15 18:00:24 +0200

    Update main.cpp

--- examples/arduino-blink/src/main.cpp
+++ examples/arduino-blink/src/main.cpp
@@ -5,10 +5,12 @@ void setup() {
   // initialize the digital pin as an output.
   pinMode(LED_BUILTIN, OUTPUT);
 }
+   
 
+     uint32_t i = 0;
 // the loop routine runs over and over again forever:
 void loop() {
-    uint32_t i = 0;    i = i + 100;
+   i = i + 100;
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(i);               // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
